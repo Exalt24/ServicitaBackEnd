af83007dd2576cd201ee46cb445fd896ccad323d
Daniel Cruz <dcruz@up.edu.ph>
2024-06-02 12:22:19 +0800

    Add existing project files to Git

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const server = require('./server');
 
 const port = process.env.PORT || 5000;
 
+
 const startServer = () => {
     server.listen(port, () => {
         console.log(`Server running on port ${port}`);
+    });
+
+    server.get('/', (req, res) => {
+        res.send('Yawa Ka World');
     });
 };
 
